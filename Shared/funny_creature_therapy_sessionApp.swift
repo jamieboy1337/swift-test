51c9032110c502919afbdf7b0c63bd48fda78f22
jamieboy1337 <jamieboy1337@gmail.com>
2022-04-22 19:36:03 -0700

    Initial Commit

---
+++ Shared/funny_creature_therapy_sessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  funny_creature_therapy_sessionApp.swift
+//  Shared
+//
+//  Created by jamie on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct funny_creature_therapy_sessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
